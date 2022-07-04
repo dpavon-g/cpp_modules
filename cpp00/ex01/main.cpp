2225e49f6cf1fd24d2a288365956a270d4ee5526
dpavon-g <daniti333@gmail.com>
2022-07-04 19:04:23 +0200

    Second ex on going

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void	print_menu()
+{
+	std::cout << "*************\n";
+	std::cout << "* 1. ADD    *\n";
+	std::cout << "* 2. SEARCH *\n";
+	std::cout << "* 3. EXIT   *\n";
+	std::cout << "*************\n";
+}
+
+int	main()
+{
+	struct datosPersona
+    {
+        string nombre;
+        char  inicial;
+        int   edad;
+        float nota;
+    };
+
+	print_menu();
+	while ()
+	(void)argc;
+	(void)argv;
+}
